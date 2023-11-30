8fe341773ce60d7edcf556d85a532fba6fa18cfd
Gino-Y <ginoyan250@gmail.com>
2023-11-30 19:04:05 +0800

    init

--- src/main/java/com/gino/springbootstart03/controller/HelloController.java
+++ src/main/java/com/gino/springbootstart03/controller/HelloController.java
@@ -1,2 +1,12 @@
-package com.gino.springbootstart03.controller;public class HelloController {
+package com.gino.springbootstart03.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello 你好";
+    }
 }
